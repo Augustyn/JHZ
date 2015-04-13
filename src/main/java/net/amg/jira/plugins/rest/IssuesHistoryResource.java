0a12b10a3bbd89829f06df1f8df52544789b6098
Ivo Zieliński <180756@edu.p.lodz.pl>
2015-04-13 18:35:29 +0200

    PIP-49 Dodano dwie metody zasobu rest oraz wyświetlanie rezultatu zapytania w GUI

--- src/main/java/net/amg/jira/plugins/rest/IssuesHistoryResource.java
+++ src/main/java/net/amg/jira/plugins/rest/IssuesHistoryResource.java
@@ -1,22 +1,118 @@
 package net.amg.jira.plugins.rest;
 
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.project.ProjectManager;
+import com.atlassian.jira.security.PermissionManager;
+import com.atlassian.jira.security.Permissions;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.util.UserUtil;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+import com.atlassian.sal.api.user.UserManager;
+import org.apache.log4j.Logger;
+import org.ofbiz.core.entity.GenericEntityException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
- * A resource of message.
+ * Resource providing all Issues in all Projects for which the requesting User has BROWSE Permission.
  */
-@Path("/message")
+@Path("/issues")
 public class IssuesHistoryResource {
 
+    private UserManager userManager;
+    private PermissionManager permissionManager;
+    private UserUtil userUtil;
+    private ProjectManager projectManager;
+    private IssueManager issueManager;
+
+    /**
+     * Used by Spring to inject dependencies
+     * @param userManager
+     * @param userUtil
+     * @param permissionManager
+     * @param projectManager
+     * @param issueManager
+     */
+    public IssuesHistoryResource(UserManager userManager, UserUtil userUtil, PermissionManager permissionManager,
+                                 ProjectManager projectManager, IssueManager issueManager) {
+        this.userManager = userManager;
+        this.userUtil = userUtil;
+        this.permissionManager = permissionManager;
+        this.projectManager = projectManager;
+        this.issueManager = issueManager;
+    }
+
+    /**
+     * Returns all Issues in all Projects for which the requesting User has BROWSE Permission. Response is an
+     * IssuesHistoryResourceModel in JSON format.
+     * @param request
+     * @return JSON representation of IssuesHistoryResource
+     */
     @GET
     @AnonymousAllowed
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response getMessage()
-    {
-       return Response.ok(new IssuesHistoryResourceModel("Hello World")).build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIssues(@Context HttpServletRequest request) {
+        String username = userManager.getRemoteUsername(request);
+        ApplicationUser user = userUtil.getUserByName(username);
+        ArrayList<Project> projects = new ArrayList<>();
+        projects.addAll(permissionManager.getProjects(Permissions.Permission.BROWSE.getId(), user));
+        ArrayList<IssueRepresentation> issues = new ArrayList<>();
+        for (Project project : projects) {
+            try {
+                for (Issue issue : issueManager.getIssueObjects(issueManager.getIssueIdsForProject(project.getId()))) {
+                    issues.add(new IssueRepresentation(issue));
+                }
+            } catch (GenericEntityException e) {
+                String message = "Unable to get Issue id for Project "+project;
+                Logger.getLogger(this.getClass()).error(message,e);
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message+" "+e).build();
+            }
+        }
+        IssuesHistoryResourceModel issuesHistoryResource = new IssuesHistoryResourceModel(issues);
+        return Response.ok(issuesHistoryResource).build();
+    }
+
+    /**
+     * Returns all Issues in Projects with given name, for which the requesting User has BROWSE Permission. Response is
+     * an IssuesHistoryResourceModel in JSON format.
+     * @param projectName
+     * @param request
+     * @return
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{projectName}")
+    public Response getProjectIssues(@PathParam("projectName") String projectName,@Context HttpServletRequest request) {
+        String username = userManager.getRemoteUsername(request);
+        ApplicationUser user = userUtil.getUserByName(username);
+        ArrayList<Project> projects = new ArrayList<>();
+        projects.addAll(permissionManager.getProjects(Permissions.Permission.BROWSE.getId(), user));
+        for(Project project : new ArrayList<>(projects)) {
+            if(!project.getName().equals(projectName)) {
+                projects.remove(project);
+            }
+        }
+        ArrayList<IssueRepresentation> issues = new ArrayList<>();
+        for (Project project : projects) {
+            try {
+                for (Issue issue : issueManager.getIssueObjects(issueManager.getIssueIdsForProject(project.getId()))) {
+                    issues.add(new IssueRepresentation(issue));
+                }
+            } catch (GenericEntityException e) {
+                String message = "Unable to get Issue id for Project "+project;
+                Logger.getLogger(this.getClass()).error(message,e);
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message+" "+e).build();
+            }
+        }
+        IssuesHistoryResourceModel issuesHistoryResource = new IssuesHistoryResourceModel(issues);
+        return Response.ok(issuesHistoryResource).build();
     }
 }
--- src/main/java/net/amg/jira/plugins/rest/IssuesHistoryResourceModel.java
+++ src/main/java/net/amg/jira/plugins/rest/IssuesHistoryResourceModel.java
@@ -1,25 +1,39 @@
 package net.amg.jira.plugins.rest;
 
-import javax.xml.bind.annotation.*;
-@XmlRootElement(name = "message")
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Represents a collection of issues created in accordance to user preferences.
+ */
+@XmlRootElement(name = "issues")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class IssuesHistoryResourceModel {
 
-    @XmlElement(name = "value")
-    private String message;
+    @XmlElement
+    private Collection<IssueRepresentation> issues = new ArrayList<>();
 
-    public IssuesHistoryResourceModel() {
+    /**
+     * Encapsulates IssueRepresentation objects.
+     * @param issues requested by gadget
+     */
+    public IssuesHistoryResourceModel(Iterable<IssueRepresentation> issues) {
+        this.issues = new HashSet<IssueRepresentation>();
+        for (IssueRepresentation representation : issues) {
+            this.issues.add(representation);
+        }
     }
 
-    public IssuesHistoryResourceModel(String message) {
-        this.message = message;
+    public Collection<IssueRepresentation> getIssues() {
+        return issues;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+    public void setIssues(Collection<IssueRepresentation> issues) {
+        this.issues = issues;
     }
 }
